e2216b60c6893ac644c35b745c3305c80078add2
r1v3r <churiver@outlook.com>
2022-07-16 00:04:57 +0800

    feat: set bottom bar

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "Hack Nerd Font Mono:size=12";
 static const char col_gray1[]       = "#222222";
