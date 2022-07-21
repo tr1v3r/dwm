e3e569c8ea6e69dee6e35dbf5cc741bd533c024e
r1v3r <churiver@outlook.com>
2022-07-22 02:47:18 +0800

    feat: accept emoji

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Hack Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "Hack Nerd Font Mono:size=12";
+static const char *fonts[]          = { "Hack Nerd Font Mono:size=14", "Noto Color Emoji:size=14" };
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
